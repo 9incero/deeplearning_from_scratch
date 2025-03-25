9da184737d58c9a879a7cee387a4e0c9f8c83ef7
y00njae <rladbswo12@gist.ac.kr>
2025-03-25 18:40:12 +0900

    feat: step6

--- steps/__init__.py
+++ steps/__init__.py
@@ -1,4 +1,5 @@
-from .step01 import Variable
-from .step02 import Function, Square
-from .step03 import Exp
+# from .step01 import Variable
+# from .step02 import Function, Square
+# from .step03 import Exp
 from .step04 import numerical_diff
+from .step06 import Variable, Function, Square, Exp
