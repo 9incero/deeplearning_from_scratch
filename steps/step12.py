e47052756d65f46e9aff6e06cb9227c93ff27d7d
y00njae <rladbswo12@gist.ac.kr>
2025-04-07 20:05:56 +0900

    feat: step12

---
+++ steps/step12.py
@@ -0,0 +1,29 @@
+from re import I
+from .step09 import Variable, as_array
+
+
+class Function:
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        for output in outputs:
+            output.set_creator(self)
+
+        self.inputs = inputs
+        self.outputs = outputs
+
+        return outputs if len(outputs) > 1 else outputs[0]
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+
+
+def add(x0, x1):
+    return Add()(x0, x1)
