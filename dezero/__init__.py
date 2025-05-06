f0e879cb076ce6702b069863a586fff0bb604b57
y00njae <rladbswo12@gist.ac.kr>
2025-05-06 17:07:07 +0900

    feat: step23

---
+++ dezero/__init__.py
@@ -0,0 +1,6 @@
+from dezero.core_simple import Variable
+from dezero.core_simple import Function
+from dezero.core_simple import using_config
+from dezero.core_simple import no_grad
+from dezero.core_simple import as_array
+from dezero.core_simple import as_variable
