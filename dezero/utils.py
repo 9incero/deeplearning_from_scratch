826e2092367fdaefc776390ac3abe1d14165dd62
9incero <9incero@gmail.com>
2025-06-19 16:21:28 +0900

    step 40~43

--- dezero/utils.py
+++ dezero/utils.py
@@ -71,11 +71,11 @@ def sum_to(x, shape):
         ndarray: Output array of the shape.
     """
     ndim = len(shape)
-    lead = x.ndim - ndim
-    lead_axis = tuple(range(lead))
-
-    axis = tuple([i + lead for i, sx in enumerate(shape) if sx == 1])
-    y = x.sum(lead_axis + axis, keepdims=True)
+    lead = x.ndim - ndim 
+    lead_axis = tuple(range(lead)) 
+    axis = tuple([i + lead for i, sx in enumerate(shape) if sx == 1]) #
+    
+    y = x.sum(lead_axis + axis, keepdims=True) 
     if lead > 0:
         y = y.squeeze(lead_axis)
     return y
